404122df901cfacd9d6fab9b0e1d60acdb871162
Name2408 <bogdanevseyv@yandex.ru>
2022-10-21 18:31:37 +0300

    first commit

---
+++ folder1/Program.cs
@@ -0,0 +1,21 @@
+﻿int GetRundomNumber(int minn, int maxn)
+{
+    int rundomNumber = new Random().Next(minn,maxn);  
+    return rundomNumber;
+}
+int rundomNumber = GetRundomNumber(10,100);
+Console.WriteLine(rundomNumber);
+
+
+int FindMaxNumber(int number)
+{
+    int max = number%10;
+    int maxtwo = number/10;
+    if (maxtwo>max)
+    {
+        max=maxtwo;
+    }
+    return max;
+}
+int max = FindMaxNumber(rundomNumber);
+Console.WriteLine(max);
---
+++ folder2/Program.cs
@@ -0,0 +1,27 @@
+﻿int GetRundomNumber(int minn, int maxn)
+{
+    int rundomNumber = new Random().Next(minn,maxn);  
+    return rundomNumber;
+}
+int rundomNumber = GetRundomNumber(100,1000);
+int number = rundomNumber;
+
+int[] array = new int[3];
+
+int count = 0;
+while(count<array.Length)
+{
+    int lastNumber = number%10;
+    array[count]=lastNumber;
+    number = number/10;
+    count = count+1;
+}
+
+//for(int i=0; i<array.Length; i++)
+
+
+
+Console.WriteLine(rundomNumber);
+Console.Write(array[2]);
+Console.WriteLine(array[0]);
+
