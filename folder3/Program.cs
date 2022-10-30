718f2e5182e76bff05295dde924245451ccc328f
Name2408 <bogdanevseyv@yandex.ru>
2022-10-31 00:43:22 +0300

    добавил решение 2 задач

---
+++ folder3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа:  ");
+
+bool x1 = int.TryParse(Console.ReadLine(), out int x);
+bool y1= int.TryParse(Console.ReadLine(), out int y);
+int quarter = 0;
+
+
+int DefinitionOfQuarter (int x, int y)
+{
+    if (x>0 && y>0)
+    quarter = 1;
+    if (x<0 && y>0)
+    quarter = 2;
+    if (x<0 && y<0)
+    quarter = 3;
+    if (x>0 && y<0)
+    quarter = 4;
+    return quarter;
+}
+
+int result = DefinitionOfQuarter(x,y);
+if (result == 0)
+{
+    Console.WriteLine("  неправильно введены координаты");
+}
+else
+{
+    Console.Write(result  );
+    Console.WriteLine(" четверть");
+}
+
---
+++ folder4/Program.cs
@@ -0,0 +1,27 @@
+﻿double StraightLength (int x1, int y1, int x2, int y2)
+{
+    double length = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+    return length;
+}
+
+
+Console.WriteLine("Введите координаты первой точки: ");
+bool a = int.TryParse(Console.ReadLine(), out int userx1);
+bool b = int.TryParse(Console.ReadLine(), out int usery1);
+
+
+Console.WriteLine("Введите координаты второй точки: ");
+bool c = int.TryParse(Console.ReadLine(), out int userx2);
+bool d = int.TryParse(Console.ReadLine(), out int usery2);
+
+double result = StraightLength(userx1, usery1, userx2, usery2);
+if (result==0)
+{
+    Console.WriteLine("error");
+}
+else
+{
+    Console.WriteLine(result);
+}
+  
+
