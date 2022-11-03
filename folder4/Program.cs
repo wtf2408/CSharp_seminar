94bc754e849501092769073ccea90442b881bb52
Name2408 <bogdanevseyv@yandex.ru>
2022-11-03 14:52:19 +0300

    add 4 и 5 задачи 3 семинара

--- folder4/Program.cs
+++ folder4/Program.cs
@@ -4,7 +4,18 @@
     return length;
 }
 
+Console.Clear();
+System.Console.WriteLine("Введите номер задачи: ");
+bool z = int.TryParse(Console.ReadLine(), out int n);
+if (n==0)
+{
+    System.Console.WriteLine("Ошибка, такого номера нет.");
+    return;
+}
 
+if (n == 21)
+{
+Console.Clear();
 Console.WriteLine("Введите координаты первой точки: ");
 bool a = int.TryParse(Console.ReadLine(), out int userx1);
 bool b = int.TryParse(Console.ReadLine(), out int usery1);
@@ -23,5 +34,14 @@ else
 {
     Console.WriteLine(result);
 }
-  
+}
+if (n == 22)
+{
+    Console.WriteLine("Hi");
+}
 
+
+else
+{
+    Console.WriteLine("Ошибка, такого номера нет.");
+}
