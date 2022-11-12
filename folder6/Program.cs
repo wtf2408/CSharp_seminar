86a479ddd7d3685298c82b3af90fb398b75e3e49
Bogdan <bogdanevseyv@example.com>
2022-11-12 16:19:31 +0300

    commit

---
+++ folder6/Program.cs
@@ -0,0 +1,25 @@
+﻿// int[] array = {-1,5,-9,4};
+
+// void ChangePositiveNumbersToNegative (int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i]*= -1;
+//     }
+// }
+
+// foreach (int item in array)
+// {
+//     Console.Write($"{item} ");
+// }
+
+// ChangePositiveNumbersToNegative(array);
+
+// Console.WriteLine();
+// foreach ( int item in array)
+// {
+//     Console.Write($"{item} ");
+// }
+
+
+bool a = int.TryParse(Console.ReadLine(), out int userx1);
